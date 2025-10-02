df32c1d16b56d0882d8d2cdcb44c0b81a571e886
iiabc <404e.abc@gmail.com>
2025-10-02 23:43:33 +0800

    refactor(core): 下放核心版本依赖至各子模块
    - 移除了主构建脚本中的 core 版本依赖声明
    - 在子模块中重新定义 core 依赖以提高灵活性
    - 新增 module-modern 子模块用于 1.21+ 版本支持
    - 调整了依赖结构以便更好地管理不同版本的核心库

--- project/common-impl/build.gradle.kts
+++ project/common-impl/build.gradle.kts
@@ -5,6 +5,8 @@ dependencies {
     compileOnly("ink.ptms:nms-all:1.0.0")
     compileOnly("ink.ptms.core:v11802:11802-minimize:mapped")
     compileOnly("ink.ptms.core:v11802:11802-minimize:universal")
+    compileOnly("ink.ptms.core:v11904:11904:mapped")
+    compileOnly("ink.ptms.core:v11200:11200")
     compileOnly("ink.ptms:Sandalphon:1.4.1")
     compileOnly("public:AttributePlus:3.2.6")
     compileOnly("public:HeadDatabase:1.3.0")
