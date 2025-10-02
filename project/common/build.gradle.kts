803a6231225e6f2c9af22a768b554ce03379b376
iiabc <404e.abc@gmail.com>
2025-10-02 23:43:37 +0800

    refactor(core): 下放核心版本依赖至各子模块
    - 移除了主构建脚本中的 core 版本依赖声明
    - 在子模块中重新定义 core 依赖以提高灵活性
    - 新增 module-modern 子模块用于 1.21+ 版本支持
    - 调整了依赖结构以便更好地管理不同版本的核心库

--- project/common/build.gradle.kts
+++ project/common/build.gradle.kts
@@ -2,6 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 taboolib { subproject = true }
 
+dependencies {
+    compileOnly("ink.ptms.core:v11904:11904:mapped")
+    compileOnly("ink.ptms.core:v11200:11200")
+}
+
 tasks.withType(KotlinCompile::class.java) {
     kotlinOptions {
         freeCompilerArgs = listOf("-module-name", "zap_common")
